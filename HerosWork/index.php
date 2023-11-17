1d57234b2991cb57c4b6589dd264c8db9d380d1a
JavierToriz <javiertoriz25@gmail.com>
2023-11-17 03:15:25 -0600

    Index

--- HerosWork/index.php
+++ HerosWork/index.php
@@ -3,48 +3,84 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Heroeswork - Encuentra tu trabajo ideal</title>
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            text-align: center;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-        }
-
-        header {
-            background-color: #919191;
-            color: white;
-            padding: 10px;
-        }
-
-        h1, h2 {
-            margin-bottom: 10px;
-        }
-
-        a {
-            display: inline-block;
-            padding: 10px 20px;
-            margin-top: 20px;
-            text-decoration: none;
-            font-size: 18px;
-            background-color: #7c7c7c;
-            color: white;
-            border-radius: 5px;
-        }
-    </style>
+    <link rel="stylesheet" href="css/index.css">
+    <title>Heroeswork</title>
+    
 </head>
 <body>
-    <header>
-        <h1>Bienvenido/a a Heroeswork</h1>
-    </header>
-    <main>
-        <h2>Encuentra el trabajo ideal para ti</h2>
-        <form action="">
-            <input type="text">
-        </form>
-        <a href="candidato/index.php">Inicia sesión</a>
-    </main>
+<header>
+    <div class="logo">
+      <img src="img/circulo.png" alt="">
+      <h2 class="nombre-empresa">Heroeswork</h2>
+
+    </div>
+    <nav>
+      <a href="#banner" class="nav-link">Iniciar sesión</a>
+      <a href="#quienSomos" class="nav-link">Información</a>
+      <a href="" class="nav-link">Contacto</a>
+    </nav>
+  </header>
+  <section id="banner">
+    <div class="contenido-banner">
+      <h1>Consigue un trabajo</h1>
+      <button>Regristrate</button>
+    </div>
+  </section>
+  <section id="quienSomos">
+    <div class="container">
+      <div class="img-container"></div>
+      <div class="texto">
+        <h2>Quienes somos?</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam doloribus quis aspernatur dolore
+          nisi ad amet dolorem, ex dignissimos eaque debitis facilis sed officiis molestias
+          accusantium saepe vero corrupti. Facilis.</p>
+      </div>
+    </div>
+    </section>
+  <section id="programas">
+    <div class="container">
+      <h2>Nuestros programas </h2>
+      <div class="programas_cartas">
+        <div class="carta">
+          <h3>Programa 1</h3>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Molestias ut quasi dolorem autem reprehenderit, repellendus ipsam, dicta eaque nihil dolores earum 
+            corporis hic esse, est similique ea soluta consectetur non?</p>
+          <button> Ver </button>  
+        </div>
+        <div class="carta">
+          <h3>Programa 2</h3>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Molestias ut quasi dolorem autem reprehenderit, repellendus ipsam, dicta eaque nihil dolores earum 
+            corporis hic esse, est similique ea soluta consectetur non?</p>
+          <button> Ver </button>  
+        </div>
+        <div class="carta">
+          <h3>Programa 3</h3>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Molestias ut quasi dolorem autem reprehenderit, repellendus ipsam, dicta eaque nihil dolores earum 
+            corporis hic esse, est similique ea soluta consectetur non?</p>
+          <button> Ver </button>  
+        </div>
+      </div>
+  </div>
+  </section >
+  <section id="caracteristicas">
+    <div class="container">
+      <ul>
+        <li>características</li>
+        <li>características</li>
+        <li>características</li>
+        <li>características</li>
+      </ul>
+    </div>
+  </section>
+  
+  <footer>
+    <div class="container">
+      <p>&copy; Heroeswork 2023</p>
+    </div>
+  </footer>
+  
 </body>
 </html>
