61e64ece04095a9e72f2f87097fbe1e930540304
Vianey González Guzmán <78153997+Vianey25@users.noreply.github.com>
2023-11-17 10:48:40 -0600

    Add files via upload

--- HerosWork/registro.php
+++ HerosWork/registro.php
@@ -1,12 +1,11 @@
-                    <div class="txt">¿Eres un trabajador?</div>
+                    <div class="txt">¿Eres una empresa?</div>
                     <p class="registro_p">Presiona <a href="empresa/formulario_registro.php" class="registro_a">aquí</a></p>
                 </div>
                 
                 <div class="txt_container">
-                    <div class="txt">¿Eres un usuario?</div>
+                    <div class="txt">¿Eres un candidato?</div>
                     <p class="registro_p">Presiona <a href="candidato/registro_candidato.php" class="registro_a">aquí</a></p>
                 </div>
                 
-                <a class="a_boton" href="index.php">
-                    <input type="button" class="boton" Value="Regresar">
-                </a>
+                <a href="index.php"><button>Volver a Inicio</button></a>
+                
