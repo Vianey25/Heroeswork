f23a727225f00d24106c22c4d1d9cc5bcdeb8f76
Fernando Antonio Vargas Velázquez <122810166+Fernando-Vargas-Velazquez@users.noreply.github.com>
2023-11-17 09:57:19 -0600

    Update index.php

--- HerosWork/empresa/index.php
+++ HerosWork/empresa/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tu Interfaz</title>
+    <title>HeroesWork</title>
     <style>
         body {
             margin: 0;
@@ -13,7 +13,7 @@
         }
 
         header {
-            background-color: #6fa5b1;
+            background-color: #9b77da;
             color: white;
             padding: 1em;
             text-align: center;
@@ -27,7 +27,7 @@
         }
 
         footer {
-            background-color: #6fa5b1;
+            background-color: #9b77da;
             color: white;
             text-align: center;
             padding: 1em;
@@ -37,7 +37,7 @@
         }
 
         .profile-button {
-            background-color: #4e6b9f;
+            background-color: #4CAF50;
             color: white;
             padding: 10px 15px;
             border: none;
@@ -78,7 +78,7 @@
         }
 
         button {
-            background-color: #4e6b9f;
+            background-color: #4caf50;
             color: white;
             padding: 10px;
             border: none;
@@ -89,53 +89,88 @@
         }
 
         button:hover {
-            background-color: #9b77da;
+            background-color: #45a049;
         }
-
-        .floating-button {
-            position: absolute;
-            bottom: 100px;
-            right: 60px;
-            background-color: #4e6b9f;
-            color: white;
-            padding: 30px;
-            border: none;
-            border-radius: 50%;
-            cursor: pointer;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-        }
-
-        .floating-button:hover {
-            background-color: #9b77da;
-        }
-
     </style>
 </head>
 <body>
 <!-- Dentro del header -->
 <header>
-    <h1>Empresa</h1>
+    <h1>Vacantes</h1>
+    <a href="test/test.php" style="color: white;">Realiza tus test</a>
     <a href="perfil.php"><button class="profile-button">Perfil</button></a>
 </header>
 
 
     <main>
-        <h2>Vacantes</h2>
+        <h2>Solicitudes Enviadas</h2>
+        
+        <?php
+
+            // Iniciar la sesión
+            session_start();
+
+            // Verificar si la sesión está configurada
+            if (isset($_SESSION['id_candidato'])) {
+                // Obtener el ID del candidato desde la sesión
+                $candidatoID = $_SESSION['id_candidato'];
+            }
+
+            // Incluir el archivo de conexión
+            include('conexion.php');
+
+            // Consulta para obtener datos de la tabla Vacantes
+            $consulta = "SELECT * FROM solicitud WHERE id_candidato = '$candidatoID'";
+            $resultado = $conexion->query($consulta);
+
+            // Mostrar los datos en la página
+            if ($resultado->num_rows > 0) {
+                echo "<ul>";
+                while ($fila = $resultado->fetch_assoc()) {
+                    // Obtener el título de la vacante mediante una segunda consulta
+                    $idVacante = $fila["id_vacante"];
+                    $consultaVacante = "SELECT titulo FROM vacantes WHERE id_vacante = '$idVacante'";
+                    $resultadoVacante = $conexion->query($consultaVacante);
+                    
+                    if ($resultadoVacante->num_rows > 0) {
+                        $filaVacante = $resultadoVacante->fetch_assoc();
+                        $tituloVacante = $filaVacante["titulo"];
+                    } else {
+                        // Manejar el caso en que no se encuentre la vacante
+                        $tituloVacante = "Vacante no encontrada";
+                    }
+
+                    // Mostrar los datos en la página
+                    echo "<li>";
+                    echo "<h3>" . $tituloVacante . "</h3>";
+                    echo "<p> Estado: " . $fila["estado"] . "</p>";
+                    echo "</li>";
+                }
+                echo "</ul>";
+            } else {
+                echo "No hay solicitudes.";
+            }
+
+            // Cerrar la conexión
+            $conexion->close();
+        ?>
+        <h2>Vacantes Disponibles</h2>
 
         <?php
-// Incluir el archivo de conexión
-include('conexion.php');
+    // Incluir el archivo de conexión
+    include('conexion.php');
 
-// Verificar si hay una sesión activa
-session_start();
+    // Obtener el id_candidato (asegúrate de tener esta variable antes de la consulta)
+    $id_candidato = $candidatoID; // Reemplaza esto con la forma en que obtienes el id_candidato
 
-// Verificar si el usuario está autenticado como empresa
-if (isset($_SESSION['id_empresa'])) {
-    // Obtener el ID de la empresa desde la sesión
-    $id_empresa = $_SESSION['id_empresa'];
+    // Consulta para obtener datos de la tabla Vacantes excluyendo las ya solicitadas por el candidato
+    $consulta = "SELECT * FROM vacantes v
+                 WHERE NOT EXISTS (
+                    SELECT 1 FROM solicitud s
+                    WHERE s.id_vacante = v.id_vacante
+                    AND s.id_candidato = $id_candidato
+                 )";
 
-    // Consulta para obtener datos de la tabla Vacantes filtrando por la empresa
-    $consulta = "SELECT * FROM Vacantes WHERE id_empresa = $id_empresa";
     $resultado = $conexion->query($consulta);
 
     // Mostrar los datos en la página
@@ -150,20 +185,16 @@ if (isset($_SESSION['id_empresa'])) {
         }
         echo "</ul>";
     } else {
-        echo "No hay vacantes disponibles para esta empresa.";
+        echo "No hay vacantes disponibles.";
     }
-} else {
-    echo "No estás autenticado como empresa.";
-}
 
-// Cerrar la conexión
-$conexion->close();
+    // Cerrar la conexión
+    $conexion->close();
 ?>
-<a href="vacantes.php" class="floating-button">&#43;</a>
+
     </main>
 
     <footer>
-        
         <p>Pie de página</p>
     </footer>
 </body>
