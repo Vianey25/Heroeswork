f12f4b0b5435f9cd0c615d672f3a71b427816926
Fernando Antonio Vargas Velázquez <122810166+Fernando-Vargas-Velazquez@users.noreply.github.com>
2023-11-17 06:04:56 -0600

    Update index.php

--- HerosWork/empresa/index.php
+++ HerosWork/empresa/index.php
@@ -1,39 +1,12 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de Empresas</title>
-    <style>
-        /* Estilos CSS aquí (puedes reutilizar algunos estilos del archivo anterior) */
-    </style>
+    <title>Document</title>
 </head>
 <body>
-    <!-- Header similar al anterior -->
-    <header>
-        <h1>Registro de Empresas</h1>
-        <a href="perfil.php"><button class="profile-button">Perfil</button></a>
-    </header>
-
-    <main>
-        <h2>Registro de Nueva Empresa</h2>
-
-        <!-- Formulario de registro de empresas -->
-        <form action="registrar_empresa.php" method="post">
-            <label for="nombre_empresa">Nombre de la Empresa:</label>
-            <input type="text" id="nombre_empresa" name="nombre_empresa" required>
-
-            <label for="descripcion_empresa">Descripción de la Empresa:</label>
-            <textarea id="descripcion_empresa" name="descripcion_empresa" rows="4" required></textarea>
-
-            <!-- Otros campos del formulario según tus necesidades -->
-
-            <button type="submit">Registrar Empresa</button>
-        </form>
-    </main>
-
-    <footer>
-        <p>Pie de página</p>
-    </footer>
+    <h1>Menú de empresa</h1>
+    <a href="vacantes.php">Agregar vacante</a>
 </body>
 </html>
