e26a61d662c826eae3e7ffe8fbcb8b4d12b15d3a
Erick <1722110095@gmail.com>
2023-11-17 11:58:01 -0600

    nueva interfaz

--- HerosWork/empresa/vacantes.php
+++ HerosWork/empresa/vacantes.php
@@ -13,7 +13,7 @@
         }
 
         header {
-            background-color: #6fa5b1   ;
+            background-color: #6fa5b1;
             color: white;
             padding: 1em;
             text-align: center;
@@ -24,7 +24,14 @@
 
         main {
             padding: 20px;
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 20px;
         }
+
         footer {
             background-color: #6fa5b1;
             color: white;
@@ -42,38 +49,29 @@
             border: none;
             border-radius: 5px;
             cursor: pointer;
+            text-decoration: none;
         }
 
         h2 {
             color: #333;
         }
 
-        ul {
-            list-style-type: none;
-            padding: 0;
-            display: flex;
-            flex-wrap: wrap;
-            gap: 20px;
+        form {
+            margin-top: 20px;
         }
 
-        li {
-            width: calc(33.33% - 20px);
-            margin-bottom: 20px;
-            border: 1px solid #ddd;
-            padding: 10px;
-            background-color: #fff;
-            border-radius: 5px;
+        label {
+            display: block;
+            margin-top: 10px;
+            font-weight: bold;
+        }
+
+        input, textarea {
+            width: 100%;
+            padding: 8px;
+            margin-top: 5px;
+            margin-bottom: 10px;
             box-sizing: border-box;
-        }
-
-        h3 {
-            font-size: 18px;
-            margin-bottom: 8px;
-        }
-
-        p {
-            font-size: 14px;
-            color: #555;
         }
 
         button {
@@ -93,103 +91,91 @@
     </style>
 </head>
 <body>
-<!-- Dentro del header -->
+
 <header>
     <h1>Vacantes</h1>
-    <a href="perfil.php"><button class="profile-button">Perfil</button></a>
+    <a href="perfil.php" class="profile-button">Perfil</a>
 </header>
 
+<main>
 
-    <main>
+    <?php
+    // Verificar si la sesión está iniciada
+    session_start();
+    // Incluir el archivo de conexión
+    include('conexion.php');
+    // Verificar si el ID de la empresa está en la sesión
+    if (isset($_SESSION['id_empresa'])) {
+        // Obtener el ID de la empresa desde la sesión
+        $id_empresa = $_SESSION['id_empresa'];
 
-        <?php
-// Verificar si la sesión está iniciada
-session_start();
-// Incluir el archivo de conexión
-include('conexion.php');
-// Verificar si el ID de la empresa está en la sesión
-if (isset($_SESSION['id_empresa'])) {
-    // Obtener el ID de la empresa desde la sesión
-    $id_empresa = $_SESSION['id_empresa'];
+        // Verificar si se ha enviado el formulario
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Conectar a la base de datos (reemplaza los valores de conexión según tu configuración)
 
-    // Verificar si se ha enviado el formulario
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Conectar a la base de datos (reemplaza los valores de conexión según tu configuración)
+            // Verificar la conexión
+            if ($conexion->connect_error) {
+                die("Error de conexión a la base de datos: " . $conexion->connect_error);
+            }
 
-        // Verificar la conexión
-        if ($conexion->connect_error) {
-            die("Error de conexión a la base de datos: " . $conexion->connect_error);
+            // Obtener los datos del formulario
+            $titulo = $_POST['titulo'];
+            $descripcion = $_POST['descripcion'];
+            $tiempo = $_POST['tiempo'];
+            $sueldo = $_POST['sueldo'];
+            $requisitos = $_POST['requisitos'];
+            $responsabilidades = $_POST['responsabilidades'];
+
+            // Preparar la consulta SQL para insertar una nueva vacante
+            $sql = "INSERT INTO Vacantes (id_empresa, titulo, descripcion, tiempo, sueldo, requisitos, responsabilidades) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+            // Preparar la declaración
+            $stmt = $conexion->prepare($sql);
+
+            // Vincular los parámetros
+            $stmt->bind_param("isssdss", $id_empresa, $titulo, $descripcion, $tiempo, $sueldo, $requisitos, $responsabilidades);
+
+            // Ejecutar la declaración
+            if ($stmt->execute()) {
+                echo "La vacante se ha guardado correctamente.";
+            } else {
+                echo "Error al guardar la vacante: " . $stmt->error;
+            }
+
+            // Cerrar la conexión
+            $stmt->close();
+            $conexion->close();
         }
-
-        // Obtener los datos del formulario
-        $titulo = $_POST['titulo'];
-        $descripcion = $_POST['descripcion'];
-        $tiempo = $_POST['tiempo'];
-        $sueldo = $_POST['sueldo'];
-        $requisitos = $_POST['requisitos'];
-        $responsabilidades = $_POST['responsabilidades'];
-
-        // Preparar la consulta SQL para insertar una nueva vacante
-        $sql = "INSERT INTO Vacantes (id_empresa, titulo, descripcion, tiempo, sueldo, requisitos, responsabilidades) VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-        // Preparar la declaración
-        $stmt = $conexion->prepare($sql);
-
-        // Vincular los parámetros
-        $stmt->bind_param("isssdss", $id_empresa, $titulo, $descripcion, $tiempo, $sueldo, $requisitos, $responsabilidades);
-
-        // Ejecutar la declaración
-        if ($stmt->execute()) {
-            echo "La vacante se ha guardado correctamente.";
-        } else {
-            echo "Error al guardar la vacante: " . $stmt->error;
-        }
-
-        // Cerrar la conexión
-        $stmt->close();
-        $conexion->close();
     }
-}
-?>
+    ?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Formulario de Vacantes</title>
-</head>
-<body>
+    <h2>Crear Nueva Vacante</h2>
 
-<h2>Crear Nueva Vacante</h2>
+    <form method="post" action="">
+        <label for="titulo">Título:</label>
+        <input type="text" name="titulo" required>
 
-<form method="post" action="">
-    <label for="titulo">Título:</label>
-    <input type="text" name="titulo" required>
+        <label for="descripcion">Descripción:</label>
+        <textarea name="descripcion" required></textarea>
 
-    <label for="descripcion">Descripción:</label>
-    <textarea name="descripcion" required></textarea>
+        <label for="tiempo">Tiempo:</label>
+        <input type="text" name="tiempo">
 
-    <label for="tiempo">Tiempo:</label>
-    <input type="text" name="tiempo">
+        <label for="sueldo">Sueldo:</label>
+        <input type="number" name="sueldo" step="0.01">
 
-    <label for="sueldo">Sueldo:</label>
-    <input type="number" name="sueldo" step="0.01">
+        <label for="requisitos">Requisitos:</label>
+        <textarea name="requisitos"></textarea>
 
-    <label for="requisitos">Requisitos:</label>
-    <textarea name="requisitos"></textarea>
+        <label for="responsabilidades">Responsabilidades:</label>
+        <textarea name="responsabilidades"></textarea>
 
-    <label for="responsabilidades">Responsabilidades:</label>
-    <textarea name="responsabilidades"></textarea>
+        <button type="submit">Guardar Vacante</button>
+    </form>
+    <a href="index.php"><button type="button">Volver</button></a>
 
-    <button type="submit">Guardar Vacante</button>
-</form>
-<a href="index.php"><button type="button">Volver</button></a>
-</body>
-</html>
+</main>
 
 
-    <footer>
-        <p>Pie de página</p>
-    </footer>
 </body>
 </html>
