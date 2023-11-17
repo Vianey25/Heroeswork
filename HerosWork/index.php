286999f7024a284a0b101e2804e613f3a4380537
Fernando Antonio Vargas Velázquez <122810166+Fernando-Vargas-Velazquez@users.noreply.github.com>
2023-11-17 00:50:24 -0600

    Add files via upload

---
+++ HerosWork/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heroeswork - Encuentra tu trabajo ideal</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            text-align: center;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #919191;
+            color: white;
+            padding: 10px;
+        }
+
+        h1, h2 {
+            margin-bottom: 10px;
+        }
+
+        a {
+            display: inline-block;
+            padding: 10px 20px;
+            margin-top: 20px;
+            text-decoration: none;
+            font-size: 18px;
+            background-color: #7c7c7c;
+            color: white;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Bienvenido/a a Heroeswork</h1>
+    </header>
+    <main>
+        <h2>Encuentra el trabajo ideal para ti</h2>
+        <form action="">
+            <input type="text">
+        </form>
+        <a href="candidato/index.php">Inicia sesión</a>
+    </main>
+</body>
+</html>
