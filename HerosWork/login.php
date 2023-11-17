06c90c04021afdc0611bd1c899a45eb1c69377e1
Erick <1722110095@gmail.com>
2023-11-17 05:50:49 -0600

    a

--- HerosWork/login.php
+++ HerosWork/login.php
@@ -1,5 +1,4 @@
 <?php
-// Establecer la conexión a la base de datos (sustituir con tus propios detalles de conexión)
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,61 +6,76 @@ $dbname = "heroeswork";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Recibir datos del formulario
 $usuario = isset($_POST['username']) ? $_POST['username'] : "";
 $contrasena = isset($_POST['password']) ? $_POST['password'] : "";
 
-// Consultar la contraseña almacenada en la base de datos para el usuario dado
-$sql = "SELECT id_candidato, contraseña FROM Candidato WHERE correo = '$usuario'";
-$result = $conn->query($sql);
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if ($result->num_rows > 0) {
-        // Obtener la información del candidato
-        $row = $result->fetch_assoc();
-        $id_candidato = $row['id_candidato'];
-        $contrasenaAlmacenada = $row['contraseña'];
+    // Autenticar en la tabla "Candidato"
+    $stmtCandidato = $conn->prepare("SELECT id_candidato, contraseña FROM Candidato WHERE correo = ?");
+    $stmtCandidato->bind_param("s", $usuario);
+    $stmtCandidato->execute();
+    $resultCandidato = $stmtCandidato->get_result();
 
-        if ($contrasena === $contrasenaAlmacenada) {
-            // Autenticación exitosa
+    if ($resultCandidato->num_rows > 0) {
+        $rowCandidato = $resultCandidato->fetch_assoc();
+        $id_candidato = $rowCandidato['id_candidato'];
+        $contrasenaCandidatoAlmacenada = $rowCandidato['contraseña'];
+
+        if ($contrasena === $contrasenaCandidatoAlmacenada) {
             session_start();
             $_SESSION['id_candidato'] = $id_candidato;
-            header("Location: candidato/"); // Redirigir a la página de inicio
+            header("Location: candidato/");
             exit();
         } else {
-            // Autenticación fallida
             echo "Credenciales incorrectas. Inténtalo de nuevo.";
         }
     } else {
-        // Usuario no encontrado en la tabla "Candidato"
         // Intentar autenticar en la tabla "Empresa"
-        $sqlEmpresa = "SELECT id_empresa, contraseña FROM Empresa WHERE correo_electronico = '$usuario'";
-        $resultEmpresa = $conn->query($sqlEmpresa);
+        $stmtEmpresa = $conn->prepare("SELECT id_empresa, contraseña FROM Empresa WHERE correo_electronico = ?");
+        $stmtEmpresa->bind_param("s", $usuario);
+        $stmtEmpresa->execute();
+        $resultEmpresa = $stmtEmpresa->get_result();
 
         if ($resultEmpresa->num_rows > 0) {
-            // Obtener la información de la empresa
             $rowEmpresa = $resultEmpresa->fetch_assoc();
             $id_empresa = $rowEmpresa['id_empresa'];
             $contrasenaEmpresaAlmacenada = $rowEmpresa['contraseña'];
 
             if ($contrasena === $contrasenaEmpresaAlmacenada) {
-                // Autenticación exitosa para la empresa
                 session_start();
                 $_SESSION['id_empresa'] = $id_empresa;
-                header("Location: empresa/index.php"); // Redirigir a la página de inicio de la empresa
+                header("Location: empresa/index.php");
                 exit();
             } else {
-                // Autenticación fallida para la empresa
                 echo "Credenciales incorrectas para la empresa. Inténtalo de nuevo.";
             }
         } else {
-            // Usuario no encontrado en la tabla "Empresa" también
-            echo "Usuario no encontrado. Inténtalo de nuevo.";
+            // Intentar autenticar en la tabla "Escuela"
+            $stmtEscuela = $conn->prepare("SELECT id_escuela, contraseña FROM Escuela WHERE correo_electronico = ?");
+            $stmtEscuela->bind_param("s", $usuario);
+            $stmtEscuela->execute();
+            $resultEscuela = $stmtEscuela->get_result();
+
+            if ($resultEscuela->num_rows > 0) {
+                $rowEscuela = $resultEscuela->fetch_assoc();
+                $id_escuela = $rowEscuela['id_escuela'];
+                $contrasenaEscuelaAlmacenada = $rowEscuela['contraseña'];
+
+                if ($contrasena === $contrasenaEscuelaAlmacenada) {
+                    session_start();
+                    $_SESSION['id_escuela'] = $id_escuela;
+                    header("Location: escuela/index.php");
+                    exit();
+                } else {
+                    echo "Credenciales incorrectas para la escuela. Inténtalo de nuevo.";
+                }
+            } else {
+                echo "Usuario no encontrado. Inténtalo de nuevo.";
+            }
         }
     }
 }
